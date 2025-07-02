15a73bc923e831493f48c007a56475513feada81
Nicholas Malart <support@ghhomesafety.com.au>
2025-07-02 10:47:58 +1000

    Changes to be committed:

---
+++ frontend/src/components/maintenance/BeepingAlarmsFiltersCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ComponentCard from '../common/ComponentCard';
+
+const BeepingAlarmsFiltersCard: React.FC = () => {
+  return (
+    <ComponentCard 
+      title="Filters"
+      desc="Filter your beeping alarms"
+    >
+      <div>
+        {/* Filter controls will go here */}
+      </div>
+    </ComponentCard>
+  );
+};
+
+export default BeepingAlarmsFiltersCard;
--- frontend/src/pages/Maintenance/BeepingAlarms.tsx
+++ frontend/src/pages/Maintenance/BeepingAlarms.tsx
@@ -1,23 +1,29 @@
 import PageBreadcrumb from "../../components/common/PageBreadCrumb";
 import PageMeta from "../../components/common/PageMeta";
 import BeepingAlarmsTable from "../../components/maintenance/BeepingAlarmsTable";
+import BeepingAlarmsFiltersCard from "../../components/maintenance/BeepingAlarmsFiltersCard";
 
 export default function BeepingAlarms() {
   return (
     <div>
       <PageMeta
-        title="React.js Blank Dashboard | TailAdmin - Next.js Admin Dashboard Template"
-        description="This is React.js Blank Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+        title="Beeping Alarms Dashboard"
+        description="View and manage beeping alarms"
       />
-      <PageBreadcrumb pageTitle="Blank Page" />
+      <PageBreadcrumb pageTitle="Beeping Alarms" />
       <div className="min-h-screen rounded-2xl border border-gray-200 bg-white px-5 py-7 dark:border-gray-800 dark:bg-white/[0.03] xl:px-10 xl:py-12">
-        <div className="mx-auto w-full text-center">
-          <h3 className="mb-4 font-semibold text-gray-800 text-theme-xl dark:text-white/90 sm:text-2xl">
-            Beeping Alarms List
-          </h3>
-          <div className="w-full overflow-x-auto custom-scrollbar">
-            <BeepingAlarmsTable />
-          </div>
+        <h3 className="mb-6 font-semibold text-gray-800 text-theme-xl dark:text-white/90 sm:text-2xl">
+          Beeping Alarms List
+        </h3>
+        
+        {/* Filters Card */}
+        <div className="mb-6">
+          <BeepingAlarmsFiltersCard />
+        </div>
+
+        {/* Table */}
+        <div className="w-full overflow-x-auto custom-scrollbar">
+          <BeepingAlarmsTable />
         </div>
       </div>
     </div>
