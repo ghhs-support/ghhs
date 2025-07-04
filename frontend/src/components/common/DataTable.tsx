6b9a14a33de54e7f2f8cb9c0457c0d0a70049f7c
Nicholas Malart <support@ghhomesafety.com.au>
2025-07-04 16:00:06 +1000

    Changes to be committed:

--- frontend/src/components/common/DataTable.tsx
+++ frontend/src/components/common/DataTable.tsx
@@ -136,7 +136,7 @@ export default function DataTable<T>({
                   onEntriesPerPageChange(e.target.value);
                   onPageChange(1);
                 }}
-                className="w-full py-2 pl-3 pr-8 text-sm text-gray-800 bg-transparent border border-gray-300 rounded-lg appearance-none dark:bg-dark-900 h-9 bg-none shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
+                className="w-full py-2 pl-3 pr-8 text-sm text-gray-800 bg-white border border-gray-300 rounded-lg appearance-none dark:bg-gray-900 h-9 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
               >
                 <option value="10" className="text-gray-500 dark:bg-gray-900 dark:text-gray-400">10</option>
                 <option value="25" className="text-gray-500 dark:bg-gray-900 dark:text-gray-400">25</option>
@@ -144,7 +144,7 @@ export default function DataTable<T>({
                 <option value="100" className="text-gray-500 dark:bg-gray-900 dark:text-gray-400">100</option>
                 <option value="200" className="text-gray-500 dark:bg-gray-900 dark:text-gray-400">200</option>
               </select>
-              <span className="absolute z-30 text-gray-500 -translate-y-1/2 right-2 top-1/2 dark:text-gray-400">
+              <span className="absolute text-gray-500 -translate-y-1/2 right-2 top-1/2 dark:text-gray-400 pointer-events-none">
                 <svg className="stroke-current" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M3.8335 5.9165L8.00016 10.0832L12.1668 5.9165" stroke="" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"></path>
                 </svg>
